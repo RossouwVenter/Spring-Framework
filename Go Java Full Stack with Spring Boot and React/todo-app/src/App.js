e9a1471dab5d6473db7a4c6830aa897708706842
Rossouw Venter <77490024+RossouwVenter@users.noreply.github.com>
2021-10-25 11:34:38 +0200

    Components, classes and functions

--- Go Java Full Stack with Spring Boot and React/todo-app/src/App.js
+++ Go Java Full Stack with Spring Boot and React/todo-app/src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        My Hello world
+        <FirstComponent ></FirstComponent>
+        <SecondComponent></SecondComponent>
+        <ThirdComponent></ThirdComponent>
+        <FourthComponent></FourthComponent>
+      </div>
+    )
+  }
 }
+
+// Class Component
+class FirstComponent extends Component{
+  render(){
+    return (
+      <div className="firstComponent">
+        FirstComponent        
+      </div>
+    )
+  }
+}
+
+class SecondComponent extends Component{
+  render(){
+    return (
+      <div className="secondComponent">
+        SecondComponent        
+      </div>
+    )
+  }
+}
+
+function ThirdComponent(){
+  return (
+    <div className="thirdComponent">
+      ThirdComponent        
+    </div>
+  )
+}
+
+function FourthComponent(){
+  return (
+    <div className="fourthComponent">
+      FourthComponent        
+    </div>
+  )
+}
+
 
 export default App;
