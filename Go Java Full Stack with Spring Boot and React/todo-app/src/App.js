0e6ff2c16391b648ed76a4f395f7f018d155a45b
Rossouw Venter <77490024+RossouwVenter@users.noreply.github.com>
2021-10-25 12:16:08 +0200

    Import and export components

--- Go Java Full Stack with Spring Boot and React/todo-app/src/App.js
+++ Go Java Full Stack with Spring Boot and React/todo-app/src/App.js
@@ -1,57 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import FirstComponent,{SecondComponent , ThirdComponent} from './components/learning-examples/FirstComponent';
+
 
 class App extends Component{
   render(){
     return (
       <div className="App">
         My Hello world
-        <FirstComponent ></FirstComponent>
+        <FirstComponent></FirstComponent>
         <SecondComponent></SecondComponent>
         <ThirdComponent></ThirdComponent>
-        <FourthComponent></FourthComponent>
+        
       </div>
     )
   }
 }
 
 // Class Component
-class FirstComponent extends Component{
-  render(){
-    return (
-      <div className="firstComponent">
-        FirstComponent        
-      </div>
-    )
-  }
-}
 
-class SecondComponent extends Component{
-  render(){
-    return (
-      <div className="secondComponent">
-        SecondComponent        
-      </div>
-    )
-  }
-}
 
-function ThirdComponent(){
-  return (
-    <div className="thirdComponent">
-      ThirdComponent        
-    </div>
-  )
-}
+// class SecondComponent extends Component{
+//   render(){
+//     return (
+//       <div className="secondComponent">
+//         SecondComponent        
+//       </div>
+//     )
+//   }
+// }
 
-function FourthComponent(){
-  return (
-    <div className="fourthComponent">
-      FourthComponent        
-    </div>
-  )
-}
+// function ThirdComponent(){
+//   return (
+//     <div className="thirdComponent">
+//       ThirdComponent        
+//     </div>
+//   )
+// }
+
+
 
 
 export default App;
