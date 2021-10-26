10645285e793f3491db8db4a54c3af8566e14277
Rossouw Venter <77490024+RossouwVenter@users.noreply.github.com>
2021-10-26 11:34:16 +0200

    increenting

--- Go Java Full Stack with Spring Boot and React/todo-app/src/components/Counter/Counter.jsx
+++ Go Java Full Stack with Spring Boot and React/todo-app/src/components/Counter/Counter.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import './Counter.css'
 
-function Counter(){
-    return (
-        <div className= "counter">
-           <button>+1</button>
-           <span className="count">0</span>
-        </div>
-    )
+class Counter extends Component{
+
+    // Define the initl state in a constructor
+    // state => counter 0
+
+    constructor(){
+        super();
+        this.state = {
+            counter : 0
+        }
+
+        this.increment = this.increment.bind(this)
+    }
+
+    render(){
+        return (
+            <div className= "counter">
+               <button onClick= {this.increment}>+1</button>
+               <span className="count">{this.state.counter}</span>
+            </div>
+        )
+    }
+
+    increment(){ // update state - counter ++
+        // console.log('increment')
+        this.state.counter++;
+        this.setState({
+            counter : this.state.counter + 1
+        })
+    }
+    
 }
+
+
 
 export default Counter
