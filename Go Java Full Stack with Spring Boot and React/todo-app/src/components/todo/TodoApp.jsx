8fce61aa6d3001f5b56629deafa4a33d6fe0c183
Rossouw Venter <77490024+RossouwVenter@users.noreply.github.com>
2021-11-11 11:19:57 +0200

    Personalizing the welcome page

--- Go Java Full Stack with Spring Boot and React/todo-app/src/components/todo/TodoApp.jsx
+++ Go Java Full Stack with Spring Boot and React/todo-app/src/components/todo/TodoApp.jsx
@@ -10,7 +10,7 @@ class TodoApp extends Component {
                         <Switch>
                         <Route path="/" exact component={LoginComponent}/>
                         <Route path="/login" component={LoginComponent}/>
-                        <Route path="/welcome" component={WelcomeComponent}/>
+                        <Route path="/welcome/:name" component={WelcomeComponent}/>
                         <Route component ={ErrorComponent}/>
                         </Switch>
                     </>
@@ -24,7 +24,7 @@ class TodoApp extends Component {
 
 class WelcomeComponent extends Component {
     render(){
-        return <div> Welcome in28minutes</div>
+        return <div> Welcome {this.props.match.params.name}</div>
     }
 }
 function ErrorComponent(){
@@ -76,7 +76,7 @@ class LoginComponent extends Component {
         // in28minutes,dummy
         if (this.state.username === 'in28minutes' && this.state.password === 'dummy') {
             // console.log('Successful')
-            this.props.history.push("/welcome")
+            this.props.history.push(`/welcome/${this.state.username}`)
             // this.setState({ showSuccessMessage: true })
             // this.setState({ hasLoginFailed: false })
 
