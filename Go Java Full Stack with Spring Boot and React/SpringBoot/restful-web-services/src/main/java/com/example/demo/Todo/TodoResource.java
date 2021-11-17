4a7ac7299b0b7093f4081d8856061ade4f102d63
Rossouw Venter <77490024+RossouwVenter@users.noreply.github.com>
2021-11-17 12:45:23 +0200

    Deleteing Todos Using Spring

--- Go Java Full Stack with Spring Boot and React/SpringBoot/restful-web-services/src/main/java/com/example/demo/Todo/TodoResource.java
+++ Go Java Full Stack with Spring Boot and React/SpringBoot/restful-web-services/src/main/java/com/example/demo/Todo/TodoResource.java
@@ -3,12 +3,16 @@ package com.example.demo.Todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Todo.Todo;
 
-
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class TodoResource {
 	
@@ -16,7 +20,19 @@ public class TodoResource {
 	private TodoHardCodedService todoService;
 	
 	@GetMapping("/users/{username}/todos")
-	public List<Todo> getAllTodos(String username){
+	public List<Todo> getAllTodos(@PathVariable String username) throws InterruptedException{
+		Thread.sleep(3000);
 		return todoService.findAll();
+	}
+	
+	@DeleteMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id){
+		
+		Todo todo = todoService.deleteById(id);
+		if (todo!=null) {
+			return ResponseEntity.noContent().build();
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 }
